8b62afe8fdbfabf7b31d11e1010fff262443d78f
Tara-1022 <taraann@karunya.edu.in>
2022-12-24 09:53:51 +0530

    Added basic working program with sample files

---
+++ palette_extraction.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PIL import Image
+from sklearn.cluster import KMeans
+
+def load_image(path):
+   image = Image.open(path).convert('RGB')
+   x = np.array(image).reshape(-1, 3) #matrix version of source image
+   img_shape = x.shape #to store the shape that can be used to reshape later
+   x_unique = np.unique(x.reshape(-1, 3), axis = 0) #to store the unique pixels
+   return image, x, x_unique, img_shape
+
+def get_palette(img):
+    kmn = KMeans(n_clusters = 8, n_init = 10)
+    kmn.fit(img_unique)
+    palette = kmn.cluster_centers_.round().astype(np.uint8)
+    palette_img = Image.fromarray(palette.reshape(-1, 1, 3))
+    return palette_img.resize((32, 8 * 32), Image.Resampling.NEAREST)
+
+source_image, matrix, img_unique, img_shape = load_image('image.jpg')
+source_image.show()
+palette = get_palette(img_unique)
+palette.show()
+palette.save("palette.png")
