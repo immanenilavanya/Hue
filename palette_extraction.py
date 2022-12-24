b6f9a9834dc8477949efa7a727347b540de53b19
Tara-1022 <taraann@karunya.edu.in>
2022-12-24 13:11:48 +0530

    Made a few modifications

--- palette_extraction.py
+++ palette_extraction.py
@@ -2,6 +2,10 @@ import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
 
+image_path = "image.jpg"
+palette_size = 8
+palette_path = "palette.png"
+
 def load_image(path):
    image = Image.open(path).convert('RGB')
    x = np.array(image).reshape(-1, 3) #matrix version of source image
@@ -9,15 +13,14 @@ def load_image(path):
    x_unique = np.unique(x.reshape(-1, 3), axis = 0) #to store the unique pixels
    return image, x, x_unique, img_shape
 
-def get_palette(img):
-    kmn = KMeans(n_clusters = 8, n_init = 10)
+def get_palette(img, palette_size):
+    kmn = KMeans(n_clusters = palette_size, n_init = 10)
     kmn.fit(img_unique)
     palette = kmn.cluster_centers_.round().astype(np.uint8)
     palette_img = Image.fromarray(palette.reshape(-1, 1, 3))
-    return palette_img.resize((32, 8 * 32), Image.Resampling.NEAREST)
+    return palette_img.resize((32, palette_size * 32), Image.Resampling.NEAREST)
 
-source_image, matrix, img_unique, img_shape = load_image('image.jpg')
-source_image.show()
-palette = get_palette(img_unique)
-palette.show()
-palette.save("palette.png")
+source_image, matrix, img_unique, img_shape = load_image(image_path)
+
+palette = get_palette(img_unique, palette_size)
+palette.save(palette_path)
