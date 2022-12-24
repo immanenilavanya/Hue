d927915a46e8f6fb5f88ce6e28cd29783cd915ec
rsumitha <sumitha132003@gmail.com>
2022-12-25 01:54:37 +0530

    Fixed the image combination format

--- image_concatenation.py
+++ image_concatenation.py
@@ -1,6 +1,10 @@
 from PIL import Image
+
 def concat(im1, im2, color=(256, 256, 256)):
-    dst = Image.new('RGB', (im1.width + im2.width, max(im1.height, im2.height)), color)
-    dst.paste(im1, (0, 0))
-    dst.paste(im2, (im1.width, 0))
-    return dst
+    new_width = im1.height // 5
+    c = new_width // im2.width
+    palette = im2.resize((im2.width * c, im1.height))
+    final = Image.new('RGB', (im1.width + palette.width, max(im1.height, palette.height)), color)
+    final.paste(im1, (0, 0))
+    final.paste(palette, (im1.width, 0))
+    return final
