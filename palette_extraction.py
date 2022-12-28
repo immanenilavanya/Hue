7eba8b3619803ae6a8c067f957f74881ab1e3080
Tara-1022 <taraann@karunya.edu.in>
2022-12-28 20:07:46 +0530

    Added logo to webpage

--- palette_extraction.py
+++ palette_extraction.py
@@ -1,17 +1,13 @@
-#The underlying image processing functions
 import numpy as np
-import streamlit as st
 from PIL import Image
 from sklearn.cluster import KMeans
 
-@st.cache
 def load_image(image):
     image = image.convert('RGB')
     temp = np.array(image).reshape(-1, 3)
     img_unique = np.unique(temp.reshape(-1, 3), axis = 0)
     return img_unique
 
-@st.cache
 def get_palette(img_unique, palette_size):
     kmn = KMeans(n_clusters = palette_size, n_init = 10)
     kmn.fit(img_unique)
@@ -19,7 +15,6 @@ def get_palette(img_unique, palette_size):
     palette_img = Image.fromarray(palette.reshape(-1, 1, 3))
     return palette_img.resize((32, palette_size * 32), Image.Resampling.NEAREST)
 
-@st.cache
 def generate_palette(image, palette_size = 8):
     img_unique = load_image(image)
     palette = get_palette(img_unique, palette_size)
--- web_page.py
+++ web_page.py
@@ -5,30 +5,33 @@ import streamlit as st
 import palette_extraction as pe
 import image_concatenation as ic
 
-@st.cache
+logo = Image.open("Hue.png")
+
 def to_bytes(image):
 	buf = BytesIO()
 	image.save(buf, format = "PNG")
 	return buf.getvalue()
 
-st.set_page_config(layout="wide", page_title="Hue - A Palette Extractor")
+st.set_page_config(layout="wide", page_title="Hue - A Palette Extractor", page_icon = logo, initial_sidebar_state = "expanded")
+st.sidebar.image(logo)
 st.write("## Generate a palette from your image")
-
-upload = st.sidebar.file_uploader("Upload an image", type = ["png", "jpg", "jpeg"])
-
-if upload is None:
-	uploaded_image = Image.open("sample_image.jpg")
-else:
-	uploaded_image = Image.open(upload)
 
 col1, col2 = st.columns(2)
 
+get_upload = st.sidebar.file_uploader("Upload an image", type = ["png", "jpg", "jpeg"])
+if get_upload is None:
+	uploaded_image = Image.open("sample_image.jpg")
+else:
+	uploaded_image = Image.open(get_upload)
+
 col1.write("Uploaded image: ")
 col1.image(uploaded_image)
-palette_size = col1.slider("Number of colours in the palette", min_value = 1, max_value = 10, value = 8, step = 1)
 
+palette_size = col1.slider("Number of colours in the palette", min_value = 1, max_value = 10, value = 8, step = 1)
 palette = pe.generate_palette(uploaded_image, palette_size)
 col2.write("Generated palette: ")
 col2.image(palette)
+
 col2.download_button("Download palette", to_bytes(palette), file_name = "palette.png")
+
 st.sidebar.download_button("Download image with palette", to_bytes(ic.concat(uploaded_image, palette)), file_name = "combined.png")
